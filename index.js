9519f7d6ee9959e2b0e86828123f5e65e913a9da
AZarbade <adwaitzarbade@gmail.com>
2024-04-12 11:31:44 +0530

    for http server

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ console.log("Quering...")
 fetch("/api/search", {
     method: 'POST',
     headers: { 'Content-Type': 'text/plain' },
-    body: 'application of ensemble training',
+    // body: 'dynamic response analysis of tube array in partially filled', // pg.27
+    body: 'back analysis of large geotechnical models', // pg.369
+    // body: 'evidence of growth of supraspinal axons', // pg.733
 }).then((response) => console.log(response))
