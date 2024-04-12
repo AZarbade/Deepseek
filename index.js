ecfb71ffa5f26bb56ef5ac01207ead746b0ef721
AZarbade <adwaitzarbade@gmail.com>
2024-04-12 20:55:36 +0530

    added query search functionality

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-console.log("Quering...")
-fetch("/api/search", {
-    method: 'POST',
-    headers: { 'Content-Type': 'text/plain' },
-    // body: 'dynamic response analysis of tube array in partially filled', // pg.27
-    body: 'back analysis of large geotechnical models', // pg.369
-    // body: 'evidence of growth of supraspinal axons', // pg.733
-}).then((response) => console.log(response))
+async function search(prompt) {
+    let results = document.getElementById("results")
+    results.innerHTML = "";
+    const response = await fetch("/api/search", {
+        method: 'POST',
+        headers: {'Content-Type': 'text/plain'},
+        body: prompt,
+    });
+    for ([path, rank] of await response.json()) {
+        let item = document.createElement("span");
+        item.appendChild(document.createTextNode(path));
+        item.appendChild(document.createElement("br"));
+        results.appendChild(item);
+    }
+}
+
+let query = document.getElementById("query");
+
+query.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        search(query.value);
+    }
+})
