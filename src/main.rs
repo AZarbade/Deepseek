13a46a69ec4d7b95dbe1948b7277630a84f8f113
AZarbade <adwaitzarbade@gmail.com>
2024-04-12 23:13:16 +0530

    factor out server modules

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,15 @@
 use poppler::PopplerDocument;
-use std::collections::HashMap;
 use std::env;
 use std::fs::{self, File};
 use std::io::BufWriter;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::process::ExitCode;
 use std::result::Result;
 use std::str;
-use tiny_http::{Header, Method, Request, Response, Server, StatusCode};
 
-struct Lexer<'a> {
-    content: &'a [char],
-}
-
-impl<'a> Lexer<'a> {
-    fn new(content: &'a [char]) -> Self {
-        Self { content }
-    }
-
-    fn trim_left(&mut self) {
-        while self.content.len() > 0 && self.content[0].is_whitespace() {
-            self.content = &self.content[1..];
-        }
-    }
-
-    fn chop(&mut self, n: usize) -> &'a [char] {
-        let token = &self.content[0..n];
-        self.content = &self.content[n..];
-        token
-    }
-
-    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char]
-    where
-        P: FnMut(&char) -> bool,
-    {
-        let mut n = 0;
-        while n < self.content.len() && predicate(&self.content[n]) {
-            n += 1;
-        }
-        self.chop(n)
-    }
-
-    fn next_token(&mut self) -> Option<String> {
-        self.trim_left();
-        if self.content.len() == 0 {
-            return None;
-        }
-
-        if self.content[0].is_numeric() {
-            return Some(self.chop_while(|x| x.is_numeric()).iter().collect());
-        }
-
-        if self.content[0].is_alphabetic() {
-            return Some(
-                self.chop_while(|x| x.is_alphabetic())
-                    .iter()
-                    .map(|x| x.to_ascii_uppercase())
-                    .collect(),
-            );
-        }
-        return Some(self.chop(1).iter().collect());
-    }
-}
-
-impl<'a> Iterator for Lexer<'a> {
-    type Item = String;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next_token()
-    }
-}
+mod model;
+use model::*;
+mod server;
 
 fn parse_entire_pdf_file(file_path: &Path) -> Result<String, ()> {
     let pdf = PopplerDocument::new_from_file(&file_path, Some("")).map_err(|err| {
@@ -89,9 +30,6 @@ fn parse_entire_pdf_file(file_path: &Path) -> Result<String, ()> {
 
     Ok(buffer)
 }
-
-type TermFreq = HashMap<String, usize>;
-type TermFreqIndex = HashMap<PathBuf, TermFreq>;
 
 fn check_index(index_path: &str) -> Result<(), ()> {
     println!("Reading {index_path} index file...");
@@ -186,99 +124,6 @@ fn usage(program: &str) {
     eprintln!("- serve <index-file> [port]: start local http server with web interface");
 }
 
-fn serve_static_file(request: Request, file_path: &str, content_type: &str) -> Result<(), ()> {
-    let content_type_header = Header::from_bytes("Content-Type", content_type).expect("some some");
-
-    let file = File::open(file_path).map_err(|err| {
-        eprintln!("ERROR: could not sever file: {file_path}: {err}");
-    })?;
-
-    let rq = Response::from_file(file).with_header(content_type_header);
-    request.respond(rq).map_err(|err| {
-        eprintln!("ERROR: could not serve static file {file_path}: {err}");
-    })
-}
-
-fn server_404(request: Request) -> Result<(), ()> {
-    request
-        .respond(Response::from_string("404").with_status_code(StatusCode(404)))
-        .map_err(|err| {
-            eprintln!("ERROR: could not serve a request: {err}");
-        })
-}
-
-fn tf(t: &str, d: &TermFreq) -> f32 {
-    let a = d.get(t).cloned().unwrap_or(0) as f32;
-    let b = d.iter().map(|(_, f)| *f).sum::<usize>() as f32;
-    a / b
-}
-
-fn idf(t: &str, d: &TermFreqIndex) -> f32 {
-    let n = d.len() as f32;
-    let m = d.values().filter(|tf| tf.contains_key(t)).count().max(1) as f32;
-    return (n / m).log10();
-}
-
-fn serve_api_request(tf_index: &TermFreqIndex, mut request: Request) -> Result<(), ()> {
-    let mut buf = Vec::new();
-    let _ = request.as_reader().read_to_end(&mut buf).map_err(|err| {
-        eprintln!("ERROR: could not read the body of the request: {err}");
-    })?;
-    let body = str::from_utf8(&buf)
-        .map_err(|err| {
-            eprintln!("ERROR: could not interpret body: {err}");
-        })?
-        .chars()
-        .collect::<Vec<_>>();
-
-    let mut result = Vec::<(&Path, f32)>::new();
-    for (path, tf_table) in tf_index {
-        let mut rank = 0f32;
-        for token in Lexer::new(&body) {
-            rank += tf(&token, &tf_table) * idf(&token, &tf_index);
-        }
-        result.push((path, rank));
-    }
-    result.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());
-    result.reverse();
-
-    let json =
-        serde_json::to_string(&result.iter().take(20).collect::<Vec<_>>()).map_err(|err| {
-            eprintln!("ERROR: could not convert search results to JSON: {err}");
-        })?;
-
-    let content_type_header = Header::from_bytes("Content-Type", "application/json")
-        .expect("That we didn't put any garbage in the headers");
-    let response = Response::from_string(&json).with_header(content_type_header);
-    let _ = request.respond(response).map_err(|err| {
-        eprintln!("ERROR: could not serve a request {err}");
-    });
-
-    Ok(())
-}
-
-fn server_request(tf_index: &TermFreqIndex, request: Request) -> Result<(), ()> {
-    println!(
-        "INFO: received request! method: {:?}, url: {:?}",
-        request.method(),
-        request.url()
-    );
-
-    match (request.method(), request.url()) {
-        (Method::Post, "/api/search") => serve_api_request(tf_index, request),
-
-        (Method::Get, "/index.js") => {
-            serve_static_file(request, "index.js", "text/javascript; charset=utf-8")
-        }
-
-        (Method::Get, "/") | (Method::Get, "/index.html") => {
-            serve_static_file(request, "index.html", "text/html; charset=utf-8")
-        }
-
-        _ => server_404(request),
-    }
-}
-
 fn entry() -> Result<(), ()> {
     let mut args = env::args();
     let program = args.next().expect("path to program is provided");
@@ -320,18 +165,8 @@ fn entry() -> Result<(), ()> {
             })?;
 
             let port = args.next().unwrap_or("9090".to_string());
-            let server = Server::http(&format!("0.0.0.0:{}", port)).map_err(|err| {
-                eprintln!("ERROR: could not start HTTP server on port {port}: {err}");
-            })?;
-
-            println!(
-                "{}",
-                format!("INFO: listining at http://localhost:{}", port)
-            );
-
-            for request in server.incoming_requests() {
-                server_request(&tf_index, request).ok();
-            }
+            let address = format!("0.0.0.0:{}", port);
+            return server::start(&address, &tf_index);
         }
         _ => {
             usage(&program);
