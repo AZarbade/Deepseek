29209b37bcc89ac04ae943ba84ee01a835514c80
AZarbade <adwaitzarbade@gmail.com>
2024-04-13 12:52:34 +0530

    added custom serve functions

--- src/server.rs
+++ src/server.rs
@@ -1,59 +1,70 @@
 use std::fs::File;
 use std::str;
+use tiny_http::{Header, Method, Request, Response, Server, StatusCode};
 
 use super::model::*;
 
-use tiny_http::{Header, Method, Request, Response, Server, StatusCode};
+fn serve_404(request: Request) -> std::io::Result<()> {
+    request.respond(Response::from_string("404").with_status_code(StatusCode(404)))
+}
 
-fn serve_static_file(request: Request, file_path: &str, content_type: &str) -> Result<(), ()> {
+fn serve_500(request: Request) -> std::io::Result<()> {
+    request.respond(Response::from_string("500").with_status_code(StatusCode(500)))
+}
+
+fn serve_400(request: Request, message: &str) -> std::io::Result<()> {
+    request
+        .respond(Response::from_string(format!("400: {message}")).with_status_code(StatusCode(400)))
+}
+
+fn serve_static_file(request: Request, file_path: &str, content_type: &str) -> std::io::Result<()> {
     let content_type_header = Header::from_bytes("Content-Type", content_type)
         .expect("That we didn't put any garbage in the headers");
-    // TODO: check if file exists and if it doesn't serve 404
-    let file = File::open(file_path).map_err(|err| {
-        eprintln!("ERROR: could not serve file {file_path}: {err}");
-    })?;
-    let response = Response::from_file(file).with_header(content_type_header);
-    request.respond(response).map_err(|err| {
-        eprintln!("ERROR: could not serve static file {file_path}: {err}");
-    })
+
+    let file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("ERROR: could not serve file {file_path}: {err}");
+            if err.kind() == std::io::ErrorKind::NotFound {
+                return serve_404(request);
+            }
+            return serve_500(request);
+        }
+    };
+    request.respond(Response::from_file(file).with_header(content_type_header))
 }
 
-fn serve_404(request: Request) -> Result<(), ()> {
-    request
-        .respond(Response::from_string("404").with_status_code(StatusCode(404)))
-        .map_err(|err| {
-            eprintln!("ERROR: could not serve a request: {err}");
-        })
-}
-
-fn serve_api_search(tf_index: &TermFreqIndex, mut request: Request) -> Result<(), ()> {
+fn serve_api_search(tf_index: &TermFreqIndex, mut request: Request) -> std::io::Result<()> {
     let mut buf = Vec::new();
-    request.as_reader().read_to_end(&mut buf).map_err(|err| {
+    if let Err(err) = request.as_reader().read_to_end(&mut buf) {
         eprintln!("ERROR: could not read the body of the request: {err}");
-    })?;
-    let body = str::from_utf8(&buf)
-        .map_err(|err| {
+        return serve_500(request);
+    }
+
+    let body = match str::from_utf8(&buf) {
+        Ok(body) => body.chars().collect::<Vec<_>>(),
+        Err(err) => {
             eprintln!("ERROR: could not interpret body as UTF-8 string: {err}");
-        })?
-        .chars()
-        .collect::<Vec<_>>();
+            return serve_400(request, "Body must be a valid UTF-8 string");
+        }
+    };
 
     let result = search_query(tf_index, &body);
 
-    let json =
-        serde_json::to_string(&result.iter().take(20).collect::<Vec<_>>()).map_err(|err| {
+    let json = match serde_json::to_string(&result.iter().take(20).collect::<Vec<_>>()) {
+        Ok(json) => json,
+        Err(err) => {
             eprintln!("ERROR: could not convert search results to JSON: {err}");
-        })?;
+            return serve_500(request);
+        }
+    };
 
     let content_type_header = Header::from_bytes("Content-Type", "application/json")
         .expect("That we didn't put any garbage in the headers");
-    let response = Response::from_string(&json).with_header(content_type_header);
-    request.respond(response).map_err(|err| {
-        eprintln!("ERROR: could not serve a request {err}");
-    })
+    request.respond(Response::from_string(&json).with_header(content_type_header))
 }
 
-fn serve_request(tf_index: &TermFreqIndex, request: Request) -> Result<(), ()> {
+fn serve_request(tf_index: &TermFreqIndex, request: Request) -> std::io::Result<()> {
     println!(
         "INFO: received request! method: {:?}, url: {:?}",
         request.method(),
@@ -80,8 +91,11 @@ pub fn start(address: &str, tf_index: &TermFreqIndex) -> Result<(), ()> {
     println!("INFO: listening at http://{address}/");
 
     for request in server.incoming_requests() {
-        // TODO: serve custom 500 in case of an error
-        serve_request(&tf_index, request).ok();
+        serve_request(&tf_index, request)
+            .map_err(|err| {
+                eprintln!("ERROR: could not serve the response: {err}");
+            })
+            .ok();
     }
 
     eprintln!("ERROR: the server socket has shutdown");
